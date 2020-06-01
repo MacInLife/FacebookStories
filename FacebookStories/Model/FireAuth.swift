644d5e1685e2c2d8579302f21bbdd60e948d4e6b
MacInLife <marie.ange.coco@gmail.com>
2020-06-01 17:14:49 +0200

    first commit

---
+++ FacebookStories/Model/FireAuth.swift
@@ -0,0 +1,9 @@
+//
+//  FireAuth.swift
+//  FacebookStories
+//
+//  Created by Marie-Ange Coco on 01/06/2020.
+//  Copyright © 2020 Marie-Ange Coco. All rights reserved.
+//
+
+import Foundation
---
+++ FacebookStories/Model/FireDB.swift
@@ -0,0 +1,9 @@
+//
+//  FireDB.swift
+//  FacebookStories
+//
+//  Created by Marie-Ange Coco on 01/06/2020.
+//  Copyright © 2020 Marie-Ange Coco. All rights reserved.
+//
+
+import Foundation
